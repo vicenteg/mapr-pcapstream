b46a427f38df8a499a40eaf859130ced5bb25230
Vince Gonzalez <vince@Vinces-MacBook-Pro.local>
2015-10-21 11:30:09 -0400

    add README, update tcpdump script

--- run_tcpdump.sh
+++ run_tcpdump.sh
@@ -1,9 +1,11 @@
-#!/bin/bash
+#!/bin/bash -ex
 
 # This works on Mac OS, probably not on linux.
 IF=$(netstat -rn | grep default | head -1 | tr -s " " | cut -f 6 -d " ")
-LIMIT=30
+FILE_LIMIT=30
+TIME_LIMIT=60
+CAPTURE_PATH="$HOME/pcap/%Y/%m/%d/dump-%H%M%S"
 
 # Create captures in a directory hierarchy for the date. Lets us find
 # captures by date later.
-sudo tcpdump -i $IF -w %Y/%m/%d/dump-%H%M%S -G 60 -W $LIMIT -s0
+sudo tcpdump -v -i $IF -w "$CAPTURE_PATH" -G $TIME_LIMIT -W $FILE_LIMIT -s0
