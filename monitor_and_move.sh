eb94e12171a0db0bd5828ae9d1a8ce62c61b593b
Vince Gonzalez <vgonzalez@mapr.com>
2015-10-26 20:09:30 +0000

    better behavior when dir does not exist

--- monitor_and_move.sh
+++ monitor_and_move.sh
@@ -2,15 +2,28 @@
 
 source env.sh
 
+inotifywait=$(which inotifywait)
+
+if [ -z $inotifywait ]; then
+    echo "inotifywait is not installed. Please install it."
+    exit 1
+fi
+
 while true; do
-    inotifywait -e create $CAPTURE_DIR
-    # get the second most recently created file
-    # the most recent is being written to and is 
-    # not ready for processing.
-    FILENAME_TO_RENAME=$(ls -atr $CAPTURE_DIR | tail -3 | head -1)
-    NEW_NAME=$(echo $FILENAME_TO_RENAME | sed -e 's/\.//g')
-    if ! $(mv "$CAPTURE_DIR/$FILENAME_TO_RENAME" "$CAPTURE_DIR/$NEW_NAME"); then
-        echo "Failed to rename $CAPTURE_DIR/$FILENAME_TO_RENAME"
+    if [ -d $CAPTURE_DIR ]; then
+        $inotifywait -e create $CAPTURE_DIR
+        # get the second most recently created file
+        # the most recent is being written to and is 
+        # not ready for processing. Filter out . and ..
+        FILENAME_TO_RENAME=$(ls -atr $CAPTURE_DIR | egrep -v '^\.{1,2}$' | tail -2 | head -1)
+        NEW_NAME=$(echo $FILENAME_TO_RENAME | sed -e 's/\.//g')
+        if ! [ $FILENAME_TO_RENAME == $NEW_NAME ]; then
+            if ! $(mv "$CAPTURE_DIR/$FILENAME_TO_RENAME" "$CAPTURE_DIR/$NEW_NAME"); then
+                echo "Failed to rename $CAPTURE_DIR/$FILENAME_TO_RENAME"
+            fi
+        fi
+    else
+        echo "waiting for $CAPTURE_DIR to exist."
     fi
     sleep .5
 done
