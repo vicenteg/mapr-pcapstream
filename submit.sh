33aa5c53df9c2cd47d2f6b32067685bb48be89cf
Vince Gonzalez <vgonzalez@mapr.com>
2015-10-30 14:45:12 -0400

    lower driver-memory

--- submit.sh
+++ submit.sh
@@ -2,10 +2,9 @@
 SPARK_SUBMIT=/opt/mapr/spark/spark-1.4.1/bin/spark-submit
 SPARK_MASTER=yarn-cluster
 $SPARK_SUBMIT \
-    --jars lib_managed/bundles/com.google.guava/guava/guava-14.0.1.jar \
     --master $SPARK_MASTER \
     --num-executors 4 \
-    --driver-memory 4g \
+    --driver-memory 2g \
     --executor-memory 2g \
     --executor-cores 1 \
     target/scala-2.10/pcapstream_2.10-1.0.jar $@
