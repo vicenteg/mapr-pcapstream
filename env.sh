a3bb5ab1437b83b52418072843d84052753b82b0
Vince Gonzalez <vicenteg@users.noreply.github.com>
2015-12-01 14:02:02 -0500

    Update env.sh
    Changed NFS directories to assume maprfs:///apps is mounted on /apps.

--- env.sh
+++ env.sh
@@ -1,17 +1,20 @@
 #!/usr/bin/env bash
 
 # This works on Mac OS, probably not on linux.
-IF=$(netstat -rn | grep default | head -1 | tr -s " " | cut -f 6 -d " ")
+# IF=$(netstat -rn | grep default | head -1 | tr -s " " | cut -f 6 -d " ")
 
 # linux
-# IF=$(netstat -rn | egrep '^(default|0.0.0.0)' | tr -s ' ' | cut -f 8 -d ' ')
+IF=$(netstat -rn | egrep '^(default|0.0.0.0)' | tr -s ' ' | cut -f 8 -d ' ')
 FILE_LIMIT=100
 TIME_LIMIT=30
 SIZE_LIMIT=512
+
+# N.B. - we are assuming that maprfs:///apps is mounted
+# on /apps on the POSIX client machines.
 MFS_CAPTURE_DIR_FORMAT="/apps/pcap/in/%Y/%m/%d"
-NFS_CAPTURE_DIR_FORMAT="/mapr/vgonzalez.spark$MFS_CAPTURE_DIR_FORMAT"
+NFS_CAPTURE_DIR_FORMAT="$MFS_CAPTURE_DIR_FORMAT"
 MFS_OUTPUT_DIR="/apps/pcap/out"
-NFS_OUTPUT_DIR="/mapr/vgonzalez.spark$MFS_OUTPUT_DIR"
+NFS_OUTPUT_DIR="$MFS_OUTPUT_DIR"
 CAPTURE_FILE_PREFIX=".dump-$(hostname)"
 CAPTURE_FILE_FORMAT="$CAPTURE_FILE_PREFIX-%H%M%S.pcap"
 NFS_CAPTURE_DIR=$(date +"$NFS_CAPTURE_DIR_FORMAT")
