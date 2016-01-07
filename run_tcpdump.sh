d932bb28cc0091e20781e92fc7c36e207205072d
Vince Gonzalez <vgonzalez@mapr.com>
2016-01-07 16:22:03 -0500

    remove size limit

--- run_tcpdump.sh
+++ run_tcpdump.sh
@@ -9,7 +9,7 @@ sudo chown -R $USER `date +$NFS_CAPTURE_DIR_FORMAT`
 # Create captures in a directory hierarchy for the date. Lets us find
 # captures by date later.
 if [ ! -z $IF ]; then
-    sudo -E tcpdump -v -i $IF -w "$CAPTURE_PATH_FORMAT" -G $TIME_LIMIT -C $SIZE_LIMIT -s0 -Z $USER
+    sudo -E tcpdump -v -i $IF -w "$CAPTURE_PATH_FORMAT" -G $TIME_LIMIT -s0 -Z $USER
 else
 	echo "No interface found."
 fi
