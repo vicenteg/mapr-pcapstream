162c11edc78d5e48059be6495c8b8a75c28d152a
Vince Gonzalez <vince@ip-192-168-1-11.ec2.internal>
2016-01-10 23:02:20 -0500

    back out dynamicAllocation, will re-add laster

--- submit.sh
+++ submit.sh
@@ -7,8 +7,6 @@ SPARK_SUBMIT=/opt/mapr/spark/spark-1.5.2/bin/spark-submit
 SPARK_MASTER=yarn-cluster
 (cd $home;
 $SPARK_SUBMIT \
-    --conf spark.dynamicAllocation.enabled=true \
-    --conf spark.shuffle.service.enabled=true \
     --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.2.0-m1.jar,lib/hadoop-pcap-lib-1.2-SNAPSHOT.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
     --master $SPARK_MASTER \
     --num-executors 4 \
