a5b043fb957e7cfe868ac24f09abd51aeee4cf14
Vince Gonzalez <vgonzalez@mapr.com>
2016-08-26 10:58:15 -0400

    update submit script

--- submit.sh
+++ submit.sh
@@ -3,13 +3,14 @@
 home=$(dirname $0)
 source "$home/env.sh"
 
-SPARK_SUBMIT=/opt/mapr/spark/spark-1.5.2/bin/spark-submit
+SPARK_VERSION=${SPARK_VERSION:=1.6.1}
+SPARK_SUBMIT=/opt/mapr/spark/spark-$SPARK_VERSION/bin/spark-submit
 SPARK_MASTER=yarn-cluster
 (cd $home;
 $SPARK_SUBMIT \
     --conf yarn.nodemanager.resource.memory-mb="$((64 * 1024))" \
     --conf yarn.nodemanager.resource.cpu-vcores="12" \
-    --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.2.0-m1.jar,lib/hadoop-pcap-lib-1.2-SNAPSHOT.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
+    --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.2.0.jar,lib/hadoop-pcap-lib-1.2-SNAPSHOT.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
     --master $SPARK_MASTER \
     --num-executors 15 \
     --executor-cores 3 \
