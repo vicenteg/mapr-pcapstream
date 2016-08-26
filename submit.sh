dd2a527101461bbe6945d1704f7fb1124e8af789
Vince Gonzalez <vgonzalez@mapr.com>
2016-08-26 14:28:59 -0400

    use serde jar with deps

--- submit.sh
+++ submit.sh
@@ -10,7 +10,7 @@ SPARK_MASTER=yarn-cluster
 $SPARK_SUBMIT \
     --conf yarn.nodemanager.resource.memory-mb="$((64 * 1024))" \
     --conf yarn.nodemanager.resource.cpu-vcores="12" \
-    --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.3.3.jar,lib/hadoop-pcap-lib-1.2-SNAPSHOT.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
+    --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.3.3.jar,lib_unmanaged/hadoop-pcap-serde-1.2-SNAPSHOT-jar-with-dependencies.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
     --master $SPARK_MASTER \
     --num-executors 15 \
     --executor-cores 3 \
