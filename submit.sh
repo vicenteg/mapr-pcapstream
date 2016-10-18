e95bd5fca7f2670e3757e0be67ab0a68ab6eea03
Vince Gonzalez <vince@Vinces-MacBook-Pro.local>
2016-10-17 22:45:43 -0400

    updating for lack of elasticsearch

--- submit.sh
+++ submit.sh
@@ -10,9 +10,9 @@ SPARK_MASTER=yarn-cluster
 $SPARK_SUBMIT \
     --conf yarn.nodemanager.resource.memory-mb="$((64 * 1024))" \
     --conf yarn.nodemanager.resource.cpu-vcores="12" \
-    --jars lib_managed/jars/org.elasticsearch/elasticsearch-spark_2.10/elasticsearch-spark_2.10-2.3.3.jar,lib_unmanaged/hadoop-pcap-serde-1.2-SNAPSHOT-jar-with-dependencies.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
+    --jars lib_unmanaged/hadoop-pcap-serde-1.2-SNAPSHOT-jar-with-dependencies.jar,lib_managed/jars/joda-time/joda-time/joda-time-2.9.1.jar \
     --master $SPARK_MASTER \
     --num-executors 15 \
     --executor-cores 2 \
     --executor-memory 3G \
-    target/scala-2.10/pcapstream_2.10-1.0.jar `date +$MFS_CAPTURE_DIR_FORMAT` $MFS_OUTPUT_DIR "$ES_HOSTS" 90)
+    target/scala-2.10/pcapstream_2.10-1.0.jar `date +$MFS_CAPTURE_DIR_FORMAT` $MFS_OUTPUT_DIR 10)
